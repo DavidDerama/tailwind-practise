d47049deb539560744d18ecb966d2024681229ff
DavidDerama <davidbrentderama@gmail.com>
2025-05-23 14:46:59 +0300

    nextjs download test

---
+++ space-test/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-black">
+      <h1 className="text-lg text-pink-400"> Hsasdas</h1>
+      <Button>TEST</Button>
+    </div>
+  );
+}
